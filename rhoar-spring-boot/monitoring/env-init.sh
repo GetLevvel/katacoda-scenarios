42d1c46dd33adbffa0d0ee85e2d919063d532505
Michael Conlon <michael.conlon@levvel.io>
2018-06-06 10:10:07 -0400

    monitoring: Deploying after launch

--- rhoar-spring-boot/monitoring/env-init.sh
+++ rhoar-spring-boot/monitoring/env-init.sh
@@ -8,10 +8,10 @@ UI_PATH=/root/code 			  # This should match your index.json key
 git clone -q https://github.com/GetLevvel/rhoar-sample-code.git
 cd ${UI_PATH} && cp -R /root/rhoar-sample-code/${PROJECT}/* ./
 
+clear # To clean up Katacoda terminal noise
+~/.launch.sh
+
 # Deploy the app to OpenShift
 oc login [[HOST_SUBDOMAIN]]-8443-[[KATACODA_HOST]].environments.katacoda.com --insecure-skip-tls-verify=true -u developer -p developer
 oc new-project dev --display-name="Dev - Spring Boot App"
 mvn package fabric8:deploy -Popenshift
-
-clear # To clean up Katacoda terminal noise
-~/.launch.sh
