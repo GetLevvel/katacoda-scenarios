9c72bf9bde18b2d08f368bec7e0391a3b3315628
Tarun Mehra <tmehra@Taruns-MacBook-Pro.local>
2018-07-17 15:28:27 -0700

    add courseData to index.json

---
+++ rhoar-vertx/portfolio-service/run-init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Standard environment initialization script. Assumes the installation path (the cp portion) has been
+# created by Katacoda via a environment.uieditorpath key. (ex: "uieditorpath": "/root/code/spring-mvc")
+
+UI_PATH=/root/code 	  # This should match your index.json key
+
+git clone -q https://github.com/tarunaz/vertx-microservices-workshop.git
+cd ${UI_PATH} && cp -R /root/vertx-microservices-workshop/* ./
+
+# Build parent project
+mvn clean install -DskipTests
+
+cd quote-generator
+mvn compile vertx:run
+
+oc login https://[[HOST_SUBDOMAIN]]-8443-[[KATACODA_HOST]].environments.katacoda.com -u developer -p developer --insecure-skip-tls-verify=true
+oc new-project vertx-micro-trader --display-name="Micro-Trader Application
+mvn fabric8:deploy -Popenshift
