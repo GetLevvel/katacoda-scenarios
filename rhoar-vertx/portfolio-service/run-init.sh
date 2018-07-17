1eb66893d86c8054b5466c08e3e5dd900ac88f96
Tarun Mehra <tmehra@Taruns-MacBook-Pro.local>
2018-07-17 15:46:59 -0700

    fix

--- rhoar-vertx/portfolio-service/run-init.sh
+++ rhoar-vertx/portfolio-service/run-init.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-# Standard environment initialization script. Assumes the installation path (the cp portion) has been
-# created by Katacoda via a environment.uieditorpath key. (ex: "uieditorpath": "/root/code/spring-mvc")
-
-UI_PATH=/root/code 	  # This should match your index.json key
-
-cd /root/code
 git clone -q https://github.com/tarunaz/vertx-microservices-workshop.git
