40c71e305716bf97ddc1c35a2cca652879d76eb5
Tarun Mehra <tmehra@Taruns-MacBook-Pro.local>
2018-07-18 07:53:29 -0700

    add bg script

--- rhoar-vertx/portfolio-service/env-init.sh
+++ rhoar-vertx/portfolio-service/env-init.sh
@@ -12,7 +12,7 @@ cd quote-generator
 mvn compile vertx:run
 
 oc login https://[[HOST_SUBDOMAIN]]-8443-[[KATACODA_HOST]].environments.katacoda.com -u developer -p developer --insecure-skip-tls-verify=true
-oc new-project vertx-micro-trader --display-name="Micro-Trader Application
+oc new-project vertx-micro-trader --display-name="Micro-Trader Application"
 mvn fabric8:deploy -Popenshift
 
 clear # To clean up Katacoda terminal noise
--- rhoar-vertx/portfolio-service/run-init.sh
+++ rhoar-vertx/portfolio-service/run-init.sh
@@ -2,4 +2,4 @@
 # Standard environment initialization script. Assumes the installation path (the cp portion) has been
 # created by Katacoda via a environment.uieditorpath key. (ex: "uieditorpath": "/root/code/spring-mvc")
 
-git clone -q https://github.com/tarunaz/vertx-microservices-workshop.git
+git clone https://github.com/tarunaz/vertx-microservices-workshop.git
