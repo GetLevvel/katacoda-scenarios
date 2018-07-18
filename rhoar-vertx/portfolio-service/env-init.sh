b3b9009ab62dcf2e3b5a2633eebd7464dcd3788d
Tarun Mehra <tmehra@Taruns-MacBook-Pro.local>
2018-07-18 08:51:00 -0700

    remove assets

--- rhoar-vertx/portfolio-service/env-init.sh
+++ rhoar-vertx/portfolio-service/env-init.sh
@@ -2,15 +2,23 @@
 # Standard environment initialization script. Assumes the installation path (the cp portion) has been
 # created by Katacoda via a environment.uieditorpath key. (ex: "uieditorpath": "/root/code/spring-mvc")
 
+UI_PATH=/root/code 	  # This should match your index.json key
+
 git clone -q https://github.com/tarunaz/vertx-microservices-workshop.git
-cd /root/code && cp -R /root/vertx-microservices-workshop/* ./
+cd ${UI_PATH} && cp -R /root/vertx-microservices-workshop/* ./
+
+# Copy quote-generator solution from previous scenario into src/
+cp -R vertx-microservices-workshop/solution/quote-generator/* vertx-microservices-workshop/quote-generator
 
 # Build parent project
-mvn clean install -DskipTests
-
-cd quote-generator
+mvn package -DskipTests
 
 oc new-project vertx-micro-trader --display-name="Micro-Trader Application"
+
+cd quote-generator
+mvn fabric8:deploy -Popenshift
+
+cd trader-dashboard
 mvn fabric8:deploy -Popenshift
 
 clear # To clean up Katacoda terminal noise
