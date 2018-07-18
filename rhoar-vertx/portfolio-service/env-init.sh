a5fa4e29561b55c0c28e5eeecf79d2f4f9d26545
Tarun Mehra <tmehra@Taruns-MacBook-Pro.local>
2018-07-18 10:42:00 -0700

    fix filepath

--- rhoar-vertx/portfolio-service/env-init.sh
+++ rhoar-vertx/portfolio-service/env-init.sh
@@ -8,7 +8,7 @@ git clone -q https://github.com/tarunaz/vertx-microservices-workshop.git
 cd ${UI_PATH} && cp -R /root/vertx-microservices-workshop/* ./
 
 # Copy quote-generator solution from previous scenario into src/
-cp -R vertx-microservices-workshop/solution/quote-generator/* vertx-microservices-workshop/quote-generator
+cp -R /root/vertx-microservices-workshop/solution/quote-generator/* quote-generator
 
 # Build parent project
 mvn install -DskipTests
@@ -18,7 +18,7 @@ oc new-project vertx-micro-trader --display-name="Micro-Trader Application"
 cd ${UI_PATH}/quote-generator
 mvn fabric8:deploy -Popenshift
 
-cd ${UI_PATH}/trader-dashboard
+cd ${UI_PATH}/  trader-dashboard
 mvn fabric8:deploy -Popenshift
 
 clear # To clean up Katacoda terminal noise
