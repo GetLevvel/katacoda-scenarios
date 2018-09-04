646dfbdec255060285475ff2a840ef4c65da2d6b
Tarun Mehra <tmehra@Taruns-MacBook-Pro.local>
2018-09-04 10:00:48 -0700

    minor

--- rhoar-vertx/quote-generator/env-init.sh
+++ rhoar-vertx/quote-generator/env-init.sh
@@ -2,7 +2,7 @@
 # Standard environment initialization script. Assumes the installation path (the cp portion) has been
 # created by Katacoda via a environment.uieditorpath key. (ex: "uieditorpath": "/root/code/spring-mvc")
 
-UI_PATH=/root/code 	  # This should match your index.json key
+UI_PATH=/root/code
 
 git clone -q https://github.com/tarunaz/vertx-kubernetes-workshop.git
 cd ${UI_PATH} && cp -R /root/vertx-kubernetes-workshop/* ./
